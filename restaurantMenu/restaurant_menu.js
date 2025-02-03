272f4f648ef3b3f39b0ff41866c16a1855e86f7f
Asma Youssef <asma.youssef@horizon-tech.tn>
2025-02-03 16:35:28 -0500

    Done <3

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$28.99', 'Oatmeal -$21.50', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $45', 'Pasta- $22.99', 'Burger- $12', 'Salmon- $25'];
+const dessertMenu = ['Cake- $11', 'Ice Cream- $9', 'Pudding- $12', 'Fruit Salad- $15'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
