f3c5ed3157fd092907e23d171c6e7c8c5045f731
Asma Youssef <asma.youssef@horizon-tech.tn>
2025-02-05 04:39:50 -0500

    Done!

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -6,10 +6,14 @@
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
     // Perform the operation
-                    let result = multiply(num1, num2);
+                    let resultmult= multiply(num1, num2);
+                    let resultdiv=div(num1,num2);
+                    let resultadd=add(num1,num2);
+                    let resultsub=sub(num1,num2);
+                    
 
                     // Display the result
-                    displayResult(result);
+                    displayResult(resultmult,resultdiv,resultadd,resultsub);
                 } else {
                     displayResult('Please enter valid numbers');
                 }
@@ -17,15 +21,36 @@
 
     function multiply(a, b) {
                 // Introduce a debugger statement to pause execution
-                debugger;
+               //// debugger;
 
                 // Multiply the numbers
                 return a * b;
     }
+    function div(a,b){
+       // debugger;
+        return a/b;    }
+    
+    function add(a,b){
+        //debugger;
+        return a+b;
+    }
+    function sub(a,b){
+        //debugger;
+        return a-b;
+    }
 
-    function displayResult(result) {
+    function displayResult(resultmult,resultdiv,resultadd,resultsub) {
                 // Display the result in the paragraph element
                 const resultElement = document.getElementById('result');
-                resultElement.textContent = `The result is: ${result}`;
+                /*resultElement.textContent = `The result is: ${resultmult}`;
+                resultElement.textContent = `The result is: ${resultdiv}`;
+                resultElement.textContent = `The result is: ${resultadd}`;
+                resultElement.textContent = `The result is: ${resultsub}`;*/
+                resultElement.innerHTML=`<strong> Results:</strong><br>
+                Addition:${resultadd}<br>
+                Substraction:${resultsub}<br>
+                Multiplication:${resultmult}<br>
+                Division:${resultdiv};
+                `
     }
         
